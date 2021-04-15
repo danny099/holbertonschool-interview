4e39f709ba67d7817996ed3171f1ea13223ca59a
Danny099 <dalmariv01@gmail.com>
2021-04-15 14:21:02 -0500

    add file

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,45 +1,38 @@
 #!/usr/bin/python3
-"""Program that reads stdin line by line and computes metrics"""
-from sys import stdin
+"""script that reads stdin line by line and computes metrics"""
+import sys
 
 
-statusCode = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-totalSize = 0
-
-
-def printStat():
-    """Function that print log accumulately"""
-    print("File size: {}".format(totalSize))
-    for x in sorted(statusCode.keys()):
-        if statusCode[x]:
-            print("{}: {}".format(x, statusCode[x]))
+def formatPrint(size, myDict):
+    """format de dictionary"""
+    print("File size: {}".format(size))
+    for key, value in sorted(statusDict.items()):
+        if (value != 0):
+            print("{}: {}".format(key, value))
 
 
 if __name__ == "__main__":
-    ct = 0
+    """init code to print the parsed data"""
+    statusList = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    statusDict = {}
+    size = 0
+    i = 0
     try:
-        for y in stdin:
-            try:
-                item = y.split()
-                totalSize += int(item[-1])
-                if item[-2] in statusCode:
-                    statusCode[item[-2]] += 1
-            except:
-                pass
-            if ct == 9:
-                printStat()
-                ct = -1
-            ct += 1
+        for line in sys.stdin:
+            i += 1
+            if (len(line.split()) < 2):
+                continue
+            lineList = line.split()
+            statusCode = lineList[-2]
+            size = size + int(lineList[-1])
+            if statusCode in statusList:
+                try:
+                    statusDict[statusCode] += 1
+                except KeyError:
+                    statusDict[statusCode] = 1
+            if i % 10 == 0:
+                formatPrint(size, statusDict)
+        formatPrint(size, statusDict)
     except KeyboardInterrupt:
-        printStat()
+        formatPrint(size, statusDict)
         raise
-    printStat()
