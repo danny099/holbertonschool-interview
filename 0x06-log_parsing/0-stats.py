26775811b57f4ff013b41d8ca50f0d6ade542127
Danny099 <dalmariv01@gmail.com>
2021-04-15 14:07:28 -0500

    add file

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,57 +1,43 @@
 #!/usr/bin/python3
-"""
-Write a script that reads stdin line by line and computes metrics:
-"""
+""" log parsing """
+
+from signal import signal, SIGINT
 import sys
 
-
-counters = {
-    "size": 0,
-    "lines": 1
-}
-
-contCode = {
-    "200": 0, "301": 0, "400": 0, "401": 0,
-    "403": 0, "404": 0, "405": 0, "500": 0
-}
-
-
-def printCodes():
-    """
-    function print the codes and the number
-    """
-    # print file size
-    print("File size: {}".format(counters["size"]))
-    # print all codes
-    for key in sorted(contCode.keys()):
-        # if a val is not 0
-        if contCode[key] != 0:
-            print("{}: {}".format(key, contCode[key]))
-
-
-def countCodeSize(listData):
-    """
-    count the codes and file size
-    """
-    # count file size
-    counters["size"] += int(listData[-1])
-    # if exists
-    if listData[-2] in contCode:
-        # count status code
-        contCode[listData[-2]] += 1
-
-
 if __name__ == "__main__":
+    i = 1
+    file_size = 0
+    status_list = {"200": 0,
+                   "301": 0,
+                   "400": 0,
+                   "401": 0,
+                   "403": 0,
+                   "404": 0,
+                   "405": 0,
+                   "500": 0}
+
     try:
         for line in sys.stdin:
             try:
-                countCodeSize(line.split(" "))
+                status_code = line.split()[-2]
+                if status_code in status_list:
+                    status_list[status_code] += 1
+                file_size += int(line.split()[-1])
+                if i % 10 == 0:
+                    print("File size: {}".format(file_size))
+                    for k, v in sorted(status_list.items()):
+                        if v:
+                            print("{}: {}".format(k, v))
+                i += 1
             except:
                 pass
-            if counters["lines"] % 10 == 0:
-                printCodes()
-            counters["lines"] += 1
     except KeyboardInterrupt:
-        printCodes()
+        print("File size: {}".format(file_size))
+        for k, v in sorted(status_list.items()):
+            if v:
+                print("{}: {}".format(k, v))
         raise
-    printCodes()
+    print("File size: {}".format(file_size))
+    for k, v in sorted(status_list.items()):
+        if v:
+            print("{}: {}".format(k, v))
